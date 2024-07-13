ce312fac63d33366f3eb2d8646a4b23f19bf1fac
Gentle-Community <justinemmanuel331@gmail.com>
2024-07-13 15:28:27 +0100

    fe

--- Ulo/bevel/routes.py
+++ Ulo/bevel/routes.py
@@ -132,7 +132,7 @@ def generate_chat():
 
         response = chat_session.send_message(content)
 
-        return jsonify({"message": response.text})
+        return response.text
     except Exception as e:
         return e
         

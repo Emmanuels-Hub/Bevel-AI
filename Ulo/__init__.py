d0878cf5bb8d7ca21443c9dbcf13aec31fc28ece
Gentle-Community <justinemmanuel331@gmail.com>
2024-08-03 14:48:57 +0100

    commit

--- Ulo/__init__.py
+++ Ulo/__init__.py
@@ -6,22 +6,15 @@ from Ulo.config import Config
 
 
 db = SQLAlchemy()
-bcrypt = Bcrypt()
-mail = Mail()
-
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
-    bcrypt.init_app(app)
-    mail.init_app(app)
 
     from Ulo.main.routes import main
-    from Ulo.bevel.routes import bevel
 
     app.register_blueprint(main)
-    app.register_blueprint(bevel)
 
     return app
--- Ulo/config.py
+++ Ulo/config.py
@@ -2,8 +2,5 @@
 class Config:
     SECRET_KEY = 'qwerty20'
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
     GEMINI_API_KEY = 'AIzaSyD_SHv2ZN4dZnfqxuxrkqqy_aaIcO85W_4'
     UPLOAD_FOLDER = 'uploads'

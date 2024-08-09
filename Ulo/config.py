9a30de0c3cbaed1f0306014218d48d5341c33667
Gentle-Community <justinemmanuel331@gmail.com>
2024-08-09 14:50:41 +0100

    commit

--- Ulo/config.py
+++ Ulo/config.py
@@ -3,3 +3,4 @@ class Config:
     SECRET_KEY = 'qwerty'
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     UPLOAD_FOLDER = 'uploads'
+    GEMINI_API_KEY = 'AIzaSyB0YbwfrLvdQSvloW9TYmoetjUXWyc415k'
